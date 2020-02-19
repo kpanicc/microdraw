16c38728bf9acc77f9f6a7ae030ef7dc72791fac
Javier Córdoba <jcordobarom@gmail.com>
2020-02-19 12:52:55 +0100

    Update server side communication code

--- app/controller/metadata/image.js
+++ app/controller/metadata/image.js
@@ -3,19 +3,19 @@ const constants = require("../../constants.js")
 
 module.exports = function (req, res, next) {
     http.get("http://" + constants.VISILABServer.IP + ":" + constants.VISILABServer.port + 
-            "/getImageMetadata", (resp) => {
-                let data = "";
-                resp.on('data', (chunk) => {
-                    data += chunk;
-                });
-
-                resp.on('end', () => {
-                    console.log(data);
-                    datajson = JSON.parse(data);
-                    res.send(datajson);
-                });
-            }).on("error", (err) => {
-                res.status(404).send("Invalid response from the VISILAB server");
+        "/getImageMetadata", (resp) => {
+            let data = "";
+            resp.on('data', (chunk) => {
+                data += chunk;
             });
-    res.send({some: "json"});
+
+            resp.on('end', () => {
+                console.log(data);
+                datajson = JSON.parse(data);
+                res.send(datajson);
+            });
+        }).on("error", (err) => {
+            console.log("ERROR getting image metadata from VISILAB server")
+            res.status(404).send("Invalid response from the VISILAB server");
+        });
 };
